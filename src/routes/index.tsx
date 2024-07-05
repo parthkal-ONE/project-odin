46b4cad0d2e6e0070b6e588070f5b417d9fa3e60
parthkal-ONE <97428134+parthkal@users.noreply.github.com>
2024-07-04 21:36:27 -0600

    Corrected Tailwind install and installed ShadCN

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,13 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import Counter from "@/components/Counter";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      <Title>ONE North America</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </main>
   );
 }
